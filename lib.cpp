d9f694fd7aaa09a226324a707f6632c85764ee53
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-01 13:52:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+char conversione (char z) {
+    char ris;
+    if (z>='a' && z<='z') {
+            ris = z-32;
+        } else if (z>='A' && z<='Z') {
+            ris = z+32;
+        } else {
+    }
+    return ris;
+}
