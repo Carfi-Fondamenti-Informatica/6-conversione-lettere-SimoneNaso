413131bd3c93b4335a248e69266c880b7f19a762
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-01 13:54:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main () {
+    char c=0;
+    cin >> c;
+    if ((c>='a' && c<='z') or (c>='A' && c<='Z')) {
+        cout << "vero" << endl;
+        cout << conversione (c) << endl;
+    } else {
+        cout << "falso" << endl;
+        cout << "errore" << endl;
+    }
+    return 0;
 }
